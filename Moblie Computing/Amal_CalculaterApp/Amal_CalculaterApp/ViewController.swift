786b461130c0487323eb8ef0328f6a461aa5cb73
HopeT93 <44265426+HopeT93@users.noreply.github.com>
2018-10-18 14:58:06 +0100

    Add files via upload

---
+++ Moblie Computing/Amal_CalculaterApp/Amal_CalculaterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amal_CalculaterApp
+//
+//  Created by at17acn on 18/10/2018.
+//  Copyright © 2018 CSStestuser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
